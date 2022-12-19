f60de4f42d0ef04ebdde04e53c3f967217331fd8
Alexander Zaraysky <azaraysky@gmail.com>
2022-12-19 22:59:12 +0300

    Graceful shutdown with logging

--- main.go
+++ main.go
@@ -9,49 +9,38 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 )
-
-// func main() {
-
-// 	http.HandleFunc("/ip", func(w http.ResponseWriter, req *http.Request) {
-// 		fmt.Fprintf(w, "%s", strings.Split(req.RemoteAddr, ":")[0])
-// 	})
-// 	http.ListenAndServe(":8090", nil)
-// }
-
-// import (
-// 	"context"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	"os/signal"
-// )
 
 func main() {
 	srv := &http.Server{
 		Addr: ":8090",
 	}
 
-	idleConnsClosed := make(chan struct{}, 1)
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-		<-sigint
-
-		if err := srv.Shutdown(context.Background()); err != nil {
-			log.Printf("HTTP server Shutdown: %v", err)
-		}
-		close(idleConnsClosed)
-	}()
-
 	http.HandleFunc("/ip", func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("%v", req)
 		fmt.Fprintf(w, "%s", strings.Split(req.RemoteAddr, ":")[0])
 	})
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-		log.Fatalf("HTTP server ListenAndServe: %v", err)
-	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started")
 
-	<-idleConnsClosed
+	<-sigint
 	log.Print("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Print("Server Exited Properly")
 }
